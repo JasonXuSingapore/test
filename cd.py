c329d173b394402b366876d0d9c00d19faadbaf4
Jason <974927201@qq.com>
2023-10-25 22:09:45 +0800

    1

---
+++ cd.py
@@ -0,0 +1,41 @@
+import turtle
+
+# 创建Turtle对象
+pen = turtle.Turtle()
+
+# 设置画笔颜色
+pen.pencolor("pink")
+
+# 设置填充颜色
+pen.fillcolor("pink")
+
+# 开始填充
+pen.begin_fill()
+
+# 设置边长（毫米）
+side_length_mm = 50
+
+# 假设屏幕DPI（像素密度）为 96，根据实际情况进行调整
+dpi = 96
+
+# 计算边长对应的像素长度
+side_length_px = side_length_mm / 25.4 * dpi
+
+# 将Turtle移动到起始位置
+pen.penup()
+pen.goto(-side_length_px / 2, 0)  # 将起点移到中心位置
+pen.pendown()
+
+# 绘制八边形
+for _ in range(8):
+    pen.forward(side_length_px)  # 向前移动边长的距离
+    pen.right(45)  # 右转45度
+
+# 结束填充
+pen.end_fill()
+
+# 隐藏Turtle箭头
+pen.hideturtle()
+
+# 关闭Turtle图形窗口
+turtle.done()
